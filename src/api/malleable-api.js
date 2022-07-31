24c7c1e9adf6d9683de6a3166f7fbab1be3ba079
Vince Rose <vrose04@gmail.com>
2022-07-31 10:43:40 -0700

    remove extra api functions added by 1.11 merge

--- src/api/malleable-api.js
+++ src/api/malleable-api.js
@@ -32,27 +32,3 @@ export function deleteMalleableProfile(id) {
     .then(({ data }) => data)
     .catch((error) => Promise.reject(handleError(error)));
 }
-
-export function createMalleableProfile(name, category, code) {
-  return axios.post('/malleable-profiles', { name, category, data: code })
-    .then(({ data }) => data)
-    .catch((error) => Promise.reject(error.response.data.error));
-}
-
-export function updateMalleableProfile(name, code) {
-  return axios.put(`/malleable-profiles/${name}`, { data: code })
-    .then(({ data }) => data)
-    .catch((error) => Promise.reject(error.response.data.error));
-}
-
-export function getMalleableProfile(name) {
-  return axios.get(`/malleable-profiles/${name}`)
-    .then(({ data }) => data)
-    .catch((error) => Promise.reject(error.response.data.error));
-}
-
-export function deleteMalleableProfile(name) {
-  return axios.delete(`/malleable-profiles/${name}`)
-    .then(({ data }) => data)
-    .catch((error) => Promise.reject(error.response.data.error));
-}
