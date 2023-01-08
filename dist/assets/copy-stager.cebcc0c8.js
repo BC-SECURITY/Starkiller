8ab5ad8c6200bbf9b58448d704c12ca1fcaa894d
Vince Rose <vrose04@gmail.com>
2023-01-08 00:44:03 -0700

    generate 2.0.0-beta4

---
+++ dist/assets/copy-stager.cebcc0c8.js
@@ -0,0 +1,1 @@
+const a={methods:{async copyStager(t){await navigator.clipboard.writeText(t),this.$snack.success("Output copied to clipboard")}}};export{a as C};
---
+++ dist/assets/index.b4f7c839.js
@@ -0,0 +1,1 @@
+import{o as n}from"./index.f33523e7.js";const t=n.extend({name:"transitionable",props:{mode:String,origin:String,transition:String}});export{t as T};
