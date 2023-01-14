a4780845025a17ed0daf5dcd9848b6937aaf85c0
Vince Rose <vrose04@gmail.com>
2023-01-13 22:03:01 -0700

    remove dist (shouldn't be on private-main)

--- dist/assets/copy-stager.cebcc0c8.js
+++
@@ -1,1 +0,0 @@
-const a={methods:{async copyStager(t){await navigator.clipboard.writeText(t),this.$snack.success("Output copied to clipboard")}}};export{a as C};
--- dist/assets/index.b4f7c839.js
+++
@@ -1,1 +0,0 @@
-import{o as n}from"./index.f33523e7.js";const t=n.extend({name:"transitionable",props:{mode:String,origin:String,transition:String}});export{t as T};
